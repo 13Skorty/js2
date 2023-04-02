c10dcfbcedc18b502392b2574a97c7579b3537ad
13Skorty <stas1989goncarov@gmail.com>
2023-04-02 15:27:35 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let number = 0 
+let n = prompt(`Напишіть любе число`)
+n = parseInt(n)
+
+for(let i = 0; i<=n; i++){
+    if( i % 2 !=0){
+        console.log(i)
+    }
+}
+/*let number = 0 
+let n = prompt(`Напишіть любе число`)
+n = parseInt(n)
+
+for(let i = 0; i< n; i+=2){
+    if( i + 2 !=0){
+        console.log(i)
+    }
+}*/
+
